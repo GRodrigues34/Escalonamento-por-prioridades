3e000c2111f5845a4934f513a13be5cfcfbdfc9e
Gustavo Rodrigues <email@gmail.com>
2024-10-12 14:07:28 -0300

    process definition

---
+++ src/Processo.java
@@ -0,0 +1,48 @@
+public class Processo {
+    private String nome;
+    private int tempoExecucao;
+    private int tempoRestante;
+    private int prioridade;
+
+
+    public Processo(String nome, int tempoExecucao, int prioridade) {
+        this.nome = nome;
+        this.tempoExecucao = tempoExecucao;
+        this.tempoRestante = tempoExecucao;
+        this.prioridade = prioridade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTempoExecucao() {
+        return tempoExecucao;
+    }
+
+    public void setTempoExecucao(int tempoExecucao) {
+        this.tempoExecucao = tempoExecucao;
+    }
+
+    public int getTempoRestante() {
+        return tempoRestante;
+    }
+
+    public void setTempoRestante(int tempoRestante) {
+        this.tempoRestante = tempoRestante;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+
+}
